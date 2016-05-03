935337637147fc75f23fe8920d8a602e780806c1
root <root@localhost.localdomain>
2016-05-03 09:45:53 +0100

    stage 4 solvestage 4 solvestage 4 solveddd

--- pwnable.kr/input/sol.c
+++ pwnable.kr/input/sol.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
 
-main(){
+void main(){
 	char *args[101];
 	int i;
 	for(i=0;i<100;i++){
@@ -15,21 +17,35 @@ main(){
 
 	int fd0[2];
 	int fd2[2];
-	int c0,c2;
 	pipe(fd0);
 	pipe(fd2);
-	close(fd0[0]);
-	close(fd2[0]);
+	
+	FILE* fp = fopen("\x0a", "ab+");
+	fwrite("\x00\x00\x00\x00", 4, 1, fp);
 
-	if(fork()==0){
-		c0=dup2(fd0[1],0);
-		c2=dup2(fd2[1],2);	
-		close(fd0[1]);
-		close(fd2[1]);
+	#define CHILD_READ0 fd0[0]
+	#define PARENT_WRITE0 fd0[1]
+	#define CHILD_READ2 fd2[0]
+	#define PARENT_WRITE2 fd2[1]
+	pid_t child;
+	if((child=fork())<0){
+		printf("Fucking bastard\n");
+	}else if(child==0){
+
+		
+		close(PARENT_WRITE0);
+		close(PARENT_WRITE2);
+		dup2(CHILD_READ0,0);
+		dup2(CHILD_READ2,2);
+		close(CHILD_READ0);
+		close(CHILD_READ2);
 		execve("./input",args,env);
 		
 	}else{
-		write(c0,"\x00\x0a\x00\xff",4);
-		write(c2,"\x00\x0a\x02\xff",4);
+		close(CHILD_READ0);
+		close(CHILD_READ2);
+		write(PARENT_WRITE0,"\x00\x0a\x00\xff",4);
+		write(PARENT_WRITE2,"\x00\x0a\x02\xff",4);
+
 	}
 }
